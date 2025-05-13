a4d4255b864893c7de32973fb84ca984638a06f1
Leonardo Heis <leonardo.heis@gmail.com>
2025-05-13 15:08:56 -0300

    adding images plus new functio in eda and main timeline

--- src/eda/eda.py
+++ src/eda/eda.py
@@ -88,3 +88,40 @@ def plot_distributions(df):
 
     plt.tight_layout()
     plt.show()
+    
+def missing_data_table(data):
+    """
+    Generate a table showing the count and percentage of missing (NaN) values in each column.
+
+    Parameters
+    data : pd.DataFrame
+        The DataFrame to analyze for missing values.
+
+    Returns
+    pd.DataFrame
+        A DataFrame containing the number and percentage of NaN values per column.
+    """
+    count = data.isna().sum()
+    percentage = round(count / len(data) * 100, 2)
+    result = {
+        'NaN Count': count,
+        'NaN Percentage (%)': percentage
+    }
+    table = pd.DataFrame(result)
+    return table
+
+def percentage_rows_missing_data(df):
+    """
+    Calculates the percentage of rows with at least one missing value.
+
+    Args:
+        df: pandas DataFrame
+
+    Returns:
+        float: Percentage of rows with missing data.
+    """
+
+    rows_with_missing_data = df.isnull().any(axis=1).sum()
+    total_rows = len(df)
+    percentage = (rows_with_missing_data / total_rows) * 100
+    return percentage
